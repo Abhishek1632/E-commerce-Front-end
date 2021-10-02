26a781905aacecbf61348d3d0220973aa02ead7f
Abhishek1632 <abhishekdaundkar4131@gmail.com>
2021-10-02 11:53:11 +0530

    primitive dada types

---
+++ Desktop/Git/app.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+console.warn("Hello World");
+console.error("Hello World");
+//string
+console.log(typeof "Hello World");
+console.log("d");
+//numbers
+console.log(typeof 1);
+console.log(typeof 1.234);
+console.log(typeof 1245698712546);
+//boolean
+console.log(typeof true);
+console.log(typeof false);
