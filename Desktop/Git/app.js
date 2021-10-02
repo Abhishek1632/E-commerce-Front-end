07a157a6f3e4d026a2c8aff46cd8026cdeb9220b
Abhishek1632 <abhishekdaundkar4131@gmail.com>
2021-10-02 13:46:33 +0530

    null vs undefined

--- Desktop/Git/app.js
+++ Desktop/Git/app.js
@@ -1,13 +1,68 @@
-console.log("Hello World");
-console.warn("Hello World");
-console.error("Hello World");
-//string
-console.log(typeof "Hello World");
-console.log("d");
-//numbers
-console.log(typeof 1);
-console.log(typeof 1.234);
-console.log(typeof 1245698712546);
-//boolean
-console.log(typeof true);
-console.log(typeof false);
+var arr = [1, 2, 3, 4];
+console.log(arr);
+arr.push(5);
+console.log(arr);
+
+console.log(arr.length);
+
+var age = 19;
+
+age = 21;
+
+console.log(age);
+
+///operators
+//Arithmetic Operators
+
+var a = 2;
+var b = 10;
+
+console.log("a + b = ", a + b);
+console.log("b -a  = ", b - a);
+console.log("b/a  = ", b / a);
+console.log("b*a  = ", b * a);
+console.log("b**a  = ", b ** a);
+console.log("b%a  = ", b % a);
+
+//Assignment Operators
+
+console.log("b  ", b);
+b++;
+console.log("b+=1 ", b);
+b -= 1;
+console.log("b-=10 ", b);
+b--;
+console.log("b--  ", b);
+
+var c = 10;
+console.warn(c++);
+console.log(c);
+//console.warn(++c);
+
+//comparison Operators
+var d = 10;
+var e = 2;
+var f = "10";
+console.log(d > e);
+
+console.log(d < e);
+
+console.log(d <= e);
+console.log(d >= e);
+console.log(d == e);
+console.log(d == f);
+console.log(d != f);
+console.warn(d == f);
+console.warn(d + f);
+
+//logical operators
+
+console.log("true && true ", true && true);
+console.log("true && false", true && false);
+console.log("false && true", true && false);
+console.log("false&& false", false && false)
+
+console.log('true   | |   true ',    true | | true)
+console.log('true   | |   false',   true | | false)
+console.log('false  | |   true',    true | | false)
+console.log('false  | |   false',   false | | false)
