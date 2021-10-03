5053f8d6792c36c382e60fb1423c3e919357b459
Abhishek1632 <abhishekdaundkar4131@gmail.com>
2021-10-03 21:38:49 +0530

    calcultor

--- Desktop/Git/app.js
+++ Desktop/Git/app.js
@@ -1,8 +1,49 @@
-var button = document.querySelector("button");
-var body = document.querySelector("body");
+const a = document.getElementsByTagName("input")[0];
+const b = document.getElementsByTagName("input")[1];
 
-const callbackfunc = (event) => {
-  body.classList.toggle("dark");
+const add = document.querySelector("#add");
+const multiply = document.querySelector("#multiply");
+const result = document.querySelector(".result");
+
+switch  (opertion) {
+  case "add":
+   resultBox.innerHTML  = parseInt(a.value)  + (parseInt(b.value);
+break
+case  "multiply":
+
+ resultBox.innerHTML  = parseInt(a.value)  *   (parseInt(b.value);
+
+break
+default:
+resultBox.innerHTML = "Not a valid opertion"
+
+
 };
 
-button.addEventListener("click", callbackfunc);
+
+// const product = () => {
+//   const result = parseInt(a.value) * parseInt(b.value);
+//   resultBox.innerHTML = result;
+// };
+
+const calculate = (event, operation) => {
+//     console.log("adding");
+//   } else  if (operation == "add") {
+//  if (opertion == "multiply") {
+//     console.log("multiply");
+//   }
+// };
+
+if (operation == "add") {
+  console.log("adding");
+} else if (operation == "multiply") {
+  console.log("multiply");
+}
+add.addEventListener("click", (Event) => {
+  calculate(Event, "add");
+});
+
+multiply.addEventListener("click", (Event) => {
+  calculate(Event, "multiply");
+});
+
